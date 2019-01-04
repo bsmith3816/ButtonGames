d9f976e69ea761c9d281aef520e81a0ca58329ad
BT_1N3_25 <@>
2019-01-04 09:28:19 -0500

    1.4

--- src/GameCode.java
+++ src/GameCode.java
@@ -3,30 +3,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameCode extends Application {
 
-    private static final int count = 10;
 
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
         primaryStage.setTitle("Fast Clicker");
         primaryStage.setScene(new Scene(root, 300, 275));
+        Text score = new Text(0,0,"Score:");
+
         primaryStage.show();
-        long start = System.nanoTime();
+
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
+                score.setText("Timer: " + (now / 1_000_000_000.0));
+                if(now / 1_000_000_000.0 == 10.0){
+                    stop();
+                }
             }
         };
         timer.start();
-
-
-    }
-
-    public void check(AnimationTimer timer, long now){
 
     }
 
